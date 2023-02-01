bd5f64eea0ed86f9cb90617cad10e4525f63eaea
Venkatesh Nagireddy <44273845+v3nk3y@users.noreply.github.com>
2023-02-01 21:29:00 +0000

    Add: Main Toolbar, Side toolbar, using material sidenav,  Use breakpointObserver for responsive toggle

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,4 +1,6 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { map, Observable, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-shell',
@@ -6,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./shell.component.scss']
 })
 export class ShellComponent {
+  // Handset Obervable to check if the device is a Handset - Responsive layout purpose
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe([Breakpoints.Handset])
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
+  constructor(private breakpointObserver: BreakpointObserver) {
+
+  }
 }
